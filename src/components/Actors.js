f6f9a1237f4f2654f371b161ff9a4eae9c9cc4cd
Elvis-Munene <gitau.elvis@student.moringaschool.com>
2022-07-27 10:21:21 +0300

    Done.

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -2,7 +2,20 @@ import React from "react";
 import { actors } from "../data";
 
 function Actors() {
-  return <div>{/*{code here}*/}</div>;
+const allactors = actors.map((actor, index) => {
+  
+return <div key={index}>
+<h3>Name:{actor.name}</h3>
+<p>Movies:</p>
+<ul>{actor.movies.map((movies,i)=> <li key={i}>{movies}</li>)}</ul>
+</div>;
+  
+})
+
+  return <div>
+    <h1>Actors Page</h1>
+    {allactors}
+  </div>;
 }
 
 export default Actors;
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,8 +1,22 @@
 import React from "react";
 import { movies } from "../data";
 
+
+
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+console.log(movies);
+const allMovies = movies.map((movie, index)=> {
+  return <div key={index}>
+    <h3>Name:{movie.title}</h3>
+    <p>Time:{movie.time}</p>
+    <p>Genres</p>
+    <ul>{movie.genres.map((genre, i)=> <li key={i}>{genre}</li>)}</ul>
+  </div>;
+});
+return <div>
+  <h1>Movies Page</h1>
+  {allMovies}
+</div>
 }
 
 export default Movies;
